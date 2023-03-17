29e6e0b9dcab600e9d2e12e500cb8bd2847afdd3
ihwang9 <ihwang9@uwo.ca>
2023-03-17 15:10:05 -0400

    Created LoginPanel and mapExplorePanels

---
+++ LoginPanel.java
@@ -0,0 +1,63 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginPanel extends JPanel implements ActionListener{
+    JButton enterButton;
+    JTextField passwordField;
+    String password;
+    
+    LoginPanel() {
+        // create JPanel for left panel and set properties
+        JPanel leftPanel = new JPanel();
+        leftPanel.setBounds(0, 0, 333, 600);
+        leftPanel.setLayout(null);
+
+        // LEFT PANEL
+        // create JLabels for password prompt and set properties
+        JLabel passwordLabel1 = new JLabel("Enter a password for admin access,");
+        passwordLabel1.setBounds(55, 250, 250, 50);
+        JLabel passwordLabel2 = new JLabel("or leave blank for general access:");
+        passwordLabel2.setBounds(55, 265, 250, 50);
+
+        // create JTextField for password input and set properties
+        passwordField = new JTextField();
+        passwordField.setBounds(55, 310, 200, 25);
+
+        // create JButton for entering password and set properties
+        enterButton = new JButton("Enter");
+        enterButton.setBounds(55, 350, 200, 25);
+        enterButton.addActionListener(this);
+
+        // create JLabel for title
+        JLabel titleLabel = new JLabel("WESTERN GIS");
+        titleLabel.setFont(new Font("MV Boli", Font.BOLD, 30));
+        titleLabel.setBounds(50, 40, 300, 40);
+
+        // create image and JLabel for Western Logo
+        JLabel logoLabel = new JLabel();
+        logoLabel.setBounds(50, 125, 200, 50);
+        ImageIcon westernLogo = new ImageIcon("./images/westernLogo.png");
+        Image logoImage = westernLogo.getImage().getScaledInstance(logoLabel.getWidth(), logoLabel.getHeight(), Image.SCALE_SMOOTH);
+        westernLogo = new ImageIcon(logoImage);
+        logoLabel.setIcon(westernLogo);
+
+        // add components to left panel
+        leftPanel.add(passwordLabel1);
+        leftPanel.add(passwordLabel2);
+        leftPanel.add(passwordField);
+        leftPanel.add(enterButton);
+        leftPanel.add(titleLabel);
+        leftPanel.add(logoLabel);
+    }
+
+    // Button event handler
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource()==enterButton) {
+            password = passwordField.getText();
+        }
+        System.out.println(password);
+    }
+}
---
+++ mapExplorePanel.java
@@ -0,0 +1,94 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
+public class mapExplorePanel {
+   JTextField searchBar;
+   
+   mapExplorePanel() {
+      // create JPanel for left panel and set properties
+      JPanel leftPanel = new JPanel();
+      leftPanel.setBounds(0, 0, 333, 600);
+      leftPanel.setLayout(null);
+
+      // create JTextField for search bar and set properties
+      searchBar = new JTextField("Search");
+      searchBar.setBounds(50, 50, 200, 25);
+      // Function to setup default text to dissappear or reappear
+      searchBar.addFocusListener(new FocusListener() {
+         public void focusGained(FocusEvent e) {
+             searchBar.setText("");
+         }
+         public void focusLost(FocusEvent e) {
+             if (searchBar.getText().isEmpty()) {
+                 searchBar.setText("Search");
+             }
+         }
+     });
+      
+      // create search button and set properties
+      JButton searchButton = new JButton();
+      searchButton.setBounds(253, 51, 22, 22);
+      ImageIcon search = new ImageIcon("./images/searchButton.jpg");
+      Image searchImage = search.getImage().getScaledInstance(searchButton.getWidth(), searchButton.getHeight(), Image.SCALE_SMOOTH);
+        search = new ImageIcon(searchImage);
+      searchButton.setIcon(search);
+      // Function to return text from the searchbar
+      searchButton.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            String searchText = searchBar.getText();
+            System.out.println("Search text: " + searchText);
+        }
+    });
+      
+      // create JLabel for POI Layers subheading and set properties
+      JLabel poiLabel = new JLabel("POI Layers");
+      poiLabel.setFont(new Font("Arial", Font.BOLD, 16));
+      poiLabel.setBounds(50, 100, 200, 25);
+
+      // create checkboxes for POI Layers and set properties
+      JCheckBox washrooms = new JCheckBox("Washrooms");
+      washrooms.setBounds(50, 150, 200, 25);
+      JCheckBox classrooms = new JCheckBox("Classrooms");
+      classrooms.setBounds(50, 175, 200, 25);
+      JCheckBox genLabs = new JCheckBox("GenLabs");
+      genLabs.setBounds(50, 200, 200, 25);
+      JCheckBox stairwells = new JCheckBox("Stairwells");
+      stairwells.setBounds(50, 225, 200, 25);
+      JCheckBox entryExits = new JCheckBox("Entry/Exits");
+      entryExits.setBounds(50, 250, 200, 25);
+      JCheckBox elevators = new JCheckBox("Elevators");
+      elevators.setBounds(50, 275, 200, 25);
+      JCheckBox userCreatedPOIs = new JCheckBox("User-created POIs");
+      userCreatedPOIs.setBounds(50, 300, 200, 25);
+      JCheckBox favourites = new JCheckBox("Favourites");
+      favourites.setBounds(50, 325, 200, 25);
+
+      // create dropdown menu for floors and set properties
+      String[] floors = {"Floor 1", "Floor 2", "Floor 3"};
+      JComboBox<String> floorDropdown = new JComboBox<>(floors);
+      floorDropdown.setBounds(50, 375, 200, 25);
+
+      // create JButton for adding POIs and set properties
+      JButton addPOIButton = new JButton("Add POI");
+      addPOIButton.setBounds(50, 425, 200, 25);
+
+      // add components to left panel
+      leftPanel.add(searchBar);
+      leftPanel.add(searchButton);
+      leftPanel.add(poiLabel);
+      leftPanel.add(washrooms);
+      leftPanel.add(classrooms);
+      leftPanel.add(genLabs);
+      leftPanel.add(stairwells);
+      leftPanel.add(entryExits);
+      leftPanel.add(elevators);
+      leftPanel.add(userCreatedPOIs);
+      leftPanel.add(favourites);
+      leftPanel.add(floorDropdown);
+      leftPanel.add(addPOIButton);
+   }
+}

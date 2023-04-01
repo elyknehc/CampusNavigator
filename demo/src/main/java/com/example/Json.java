b6a6009c5d7402b60d4f12a208b52dcee374e1d0
Michael Gao <mgao244@uwo.ca>
2023-04-01 16:32:49 -0400

    Reading from and writing to JSON file

--- demo/src/main/java/com/example/Json.java
+++ demo/src/main/java/com/example/Json.java
@@ -6,7 +6,6 @@ import java.nio.file.*;
 import java.util.*;
 
 class Json {
-<<<<<<< HEAD
 
    public static String readFile() {
       String data = "";
@@ -34,51 +33,5 @@ class Json {
       String data = readFile();
       System.out.println(data);
       writeFile(data);
-=======
-   public static void main(String[] args) {
-      System.out.println("hello");
-
-      /* 
-      JSONObject jo = new JSONObject();
-      jo.put("name", "jon doe");
-      jo.put("age", "22");
-      jo.put("city", "chicago");
-      System.out.println(jo.toString());
-      */
-      /*ArrayList<POI> result = new ArrayList<POI>();
-      
-        try{
-            String text = new String(Files.readAllBytes(Paths.get("POIData.json")), "StandardCharsets.UTF_8");
-      
-            JSONObject obj = new JSONObject(text);
-            JSONArray arr = obj.getJSONArray("employees");
-      
-            for(int i = 0; i < arr.length(); i++){
-                String name = arr.getJSONObject(i).getString("name");
-                short salary = Short.parseShort(arr.getJSONObject(i).getString("salary"));
-                String position = arr.getJSONObject(i).getString("position");
-                byte years_in_company = Byte.parseByte(arr.getJSONObject(i).getString("years_in_company")); 
-                if (position.compareToIgnoreCase("manager") == 0){
-                    result.add(new Manager(name, salary, position, years_in_company));
-                }
-                else{
-                    result.add(new OrdinaryEmployee(name, salary, position, years_in_company));
-                }
-            }           
-        }
-        catch(Exception ex){
-            System.out.println(ex.toString());
-        }
-        */
-      String resourceName = "/POIData.json";
-      InputStream is = Json.class.getResourceAsStream(resourceName);
-      if (is == null) {
-         throw new NullPointerException("Cannot find resource file " + resourceName);
-      }
-
-      JSONTokener tokener = new JSONTokener(is);
-      JSONObject object = new JSONObject(tokener);
-      System.out.println(object.toString());
->>>>>>> e13141ef9531e6b891ed6efa8983fa18ee1bd4bb
    }
 }

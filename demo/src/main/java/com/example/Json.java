626c1e81f93bcc9f063c01af00bf4c7fa11d8812
Arsalaan Ali <arsalaanali2@gmail.com>
2023-04-01 13:50:56 -0400

    JSON F**ING WORKS

--- demo/src/main/java/com/example/Json.java
+++ demo/src/main/java/com/example/Json.java
@@ -1,19 +1,14 @@
 package com.example;
 
+import org.json.JSONObject;
 
 class Json {
     public static void main(String[] args) {
-      
-      try{
-
-      obj.put("name", "foo");
-      obj.put("num", 100);
-      obj.put("balance", 1000.21);
-      obj.put("is_vip", true);
-      }
-      catch (Exception e) {
-        System.out.println(e);
-    }
-      System.out.print(obj);
+      System.out.println("hello");
+      JSONObject jo = new JSONObject();
+      jo.put("name", "jon doe");
+      jo.put("age", "22");
+      jo.put("city", "chicago");
+      System.out.println(jo.toString());
    }
 }

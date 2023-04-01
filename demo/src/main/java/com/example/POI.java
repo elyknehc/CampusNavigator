97f6575dc7c70750d26ae049a2853962cf35ae72
Arsalaan Ali <arsalaanali2@gmail.com>
2023-04-01 15:29:12 -0400

    added POI encoding and decoding

--- demo/src/main/java/com/example/POI.java
+++ demo/src/main/java/com/example/POI.java
@@ -1,26 +1,47 @@
 package com.example;
 
+import org.json.JSONObject;
+
+
 public class POI {
     private String name, description, category;
     private int ID;
     private boolean isFavourite, isUser;
 
     public static void main(String[] args) {
-        POI x = new POI("null", "null", "null",5);
-        System.out.println(x);
+        POI x = new POI("hey", "yo", "what", 5, true, true);
+        System.out.println(POI.toPOI(x.stringify()));
     }
+    
+    
 
-    public POI(String name, String description, String category, int ID) {
+    public POI(String name, String description, String category, int ID, boolean isFavourite, boolean isUser) {
         this.name = name;
         this.description = description;
         this.category = category;
         this.ID = ID;
+        this.isFavourite = isFavourite;
+        this.isUser = isUser;
     }
 
     @Override
     public String toString() {
         return "POI Info:\n name: " + name + "\n" + " description: " + description + "\n" + " category: " + category
                 + "\n" + " isFavourite: " + isFavourite + "\n" + " isUser: " + isUser + "\n" + " id: " + ID;
+    }
+
+    public String stringify() {
+        JSONObject temp = new JSONObject(this);
+        System.out.println(temp);
+        return temp.toString();
+    }
+
+    public static POI toPOI(String json) {
+        JSONObject tempJson = new JSONObject(json);
+        System.out.println(tempJson.get("name"));
+        return new POI((String) tempJson.get("name"), (String) tempJson.get("description"), (String) tempJson.get("category"), (int) tempJson.get("ID"), (boolean) tempJson.get("isFavourite"), (boolean) tempJson.get("isUser"));
+        // POI temp = new POI();
+
     }
     
 
@@ -56,11 +77,19 @@ public class POI {
         this.ID = ID;
     }
 
-    public boolean getisFavourite() {
+    public boolean getIsFavourite() {
         return this.isFavourite;
     }
 
-    public void setisFavourite(boolean isFavourite) {
+    public void setIsFavourite(boolean isFavourite) {
+        this.isFavourite = isFavourite;
+    }
+
+    public boolean getIsUser() {
+        return this.isFavourite;
+    }
+
+    public void setIsUser(boolean isFavourite) {
         this.isFavourite = isFavourite;
     }
 
--- demo/src/main/java/com/example/User.java
+++ demo/src/main/java/com/example/User.java
@@ -1,3 +1,4 @@
+package com.example;
 import java.util.*;
 
 public class User {

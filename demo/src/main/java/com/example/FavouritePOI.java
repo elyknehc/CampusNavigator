3d91584e60c8d8e087e567562ff15fe0179a1e3d
ihwang9 <ihwang9@uwo.ca>
2023-04-01 20:58:53 -0400

    fixed errors from previous merge

--- demo/src/main/java/com/example/FavouritePOI.java
+++ demo/src/main/java/com/example/FavouritePOI.java
@@ -1,6 +1,6 @@
 package com.example;
 public class FavouritePOI extends POI {
-    public FavouritePOI(String name, String description, String category, int ID) {
-        super(name, description, category, ID);
+    public FavouritePOI(String name, String description, String category, int ID, int x, int y, int roomNum, int floor, boolean isFavourite, boolean isUser) {
+        super(name, description, category, ID, x, y, roomNum, floor, isFavourite, isUser);
     }
 }
--- demo/src/main/java/com/example/UserPOI.java
+++ demo/src/main/java/com/example/UserPOI.java
@@ -1,6 +1,6 @@
 package com.example;
 public class UserPOI extends POI {
-    public UserPOI(String name, String description, String category, int ID) {
-        super(name, description, category, ID);
+    public UserPOI(String name, String description, String category, int ID, int x, int y, int roomNum, int floor, boolean isFavourite, boolean isUser) {
+        super(name, description, category, ID, x, y, roomNum, floor, isFavourite, isUser);
     }
 }

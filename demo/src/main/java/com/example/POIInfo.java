adde9662204f73e62233dee9cdf876bf2bf56b9b
Kevin Chau <kchau57@uwo.ca>
2023-04-03 17:25:59 -0400

    Changes to login mapexplore, weather, and POIInfo

--- demo/src/main/java/com/example/POIInfo.java
+++ demo/src/main/java/com/example/POIInfo.java
@@ -1,36 +1,47 @@
 package com.example;
 import java.awt.Font;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 /**
  * This class is responsible for showing information aout the current POI and offering admins to chance to edit them
  * @author Kevin
  */
 
-public class POIInfo extends JFrame {
+public class POIInfo extends JFrame{
+
 	// Create a new frame and pass in a new POI object through it
     POIInfo(POI current) {
+		final POI curr = current;
 		// Create the labels and get information from current 
 		JLabel title = new JLabel("Point of Interest Information");
-		title.setBounds(75, 20, 340, 60);
-		JLabel poiName = new JLabel("Name: Example");
-		poiName.setBounds(75, 70, 340, 60);
-		JLabel building = new JLabel("Building:");
-		building.setBounds(75, 120, 340, 60);
-		JLabel type = new JLabel("Type: Example");
-		type.setBounds(75, 170, 170, 60);
-		JLabel description = new JLabel("Description: Example");
-		description.setBounds(75, 220, 340, 60);
-		JLabel floor = new JLabel("Floor: Example");
-		floor.setBounds(75, 270, 340, 60);
-		JLabel favourite = new JLabel("Favourite: ");
-		favourite.setBounds(75, 320, 100, 60);
+		title.setBounds(75, 20, 400, 60);
+		JLabel poiName = new JLabel("Name: " + curr.getName());
+		poiName.setBounds(75, 70, 400, 60);
+		JLabel building = new JLabel("Building: " + curr.getBuilding());
+		building.setBounds(75, 120, 400, 60);
+		JLabel type = new JLabel("Type: " + curr.getDescription());
+		type.setBounds(75, 170, 400, 60);
+		JLabel description = new JLabel("Description: " + curr.getDescription());
+		description.setBounds(75, 220, 400, 60);
+		JLabel floor = new JLabel("Floor: " + curr.getFloor());
+		floor.setBounds(75, 270, 400, 60);
+		JLabel favourite = new JLabel("Favourite: " + curr.getIsFavourite());
+		favourite.setBounds(75, 320, 200, 60);
 		JToggleButton favouriteToggle = new JToggleButton("Favourite");
-		favouriteToggle.setBounds(190, 340, 100, 30);
+		favouriteToggle.setBounds(220, 340, 100, 30);
 		JLabel adminEdit = new JLabel("Admin Edit");
 		adminEdit.setBounds(75, 390, 150, 30);
 		JButton edit = new JButton("Edit");
-		edit.setBounds(190, 392, 100, 30);
+		edit.setBounds(220, 392, 100, 30);
+
+		favouriteToggle.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				curr.setIsFavourite(true);
+			}
+		});
 
 
 		// Set the font for each label
@@ -62,4 +73,5 @@ public class POIInfo extends JFrame {
 		this.add(edit);
 		this.setVisible(true);
     }
+
 }

d4188d1707723315e9eeaa225044ae1c446038b8
Arsalaan Ali <arsalaanali2@gmail.com>
2023-04-04 05:46:15 -0400

    remove test poi

--- demo/src/main/java/com/example/MapScrollPanel.java
+++ demo/src/main/java/com/example/MapScrollPanel.java
@@ -38,7 +38,6 @@ public class MapScrollPanel extends JPanel {
 
 
         // TEST CODE
-        User.addToAllPOI(new POI("testPOI", "testDescrip", "classrooms", "MC", 1, 100, 100, 201, 3, false, false));
         for (POI poi : User.getFilteredPOI()) {
             MapPOI tempPin = new MapPOI(poi);
             poiLayer.addPOItoMap(tempPin);
